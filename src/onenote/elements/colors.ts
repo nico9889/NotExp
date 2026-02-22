ad636fcff70dfae450a604793178c181537ceb87
Nicola C. <nx9889@gmail.com>
2026-02-22 20:08:05 +0100

    feat: experimental dark mode color inversion

--- src/onenote/elements/colors.ts
+++ src/onenote/elements/colors.ts
@@ -7,14 +7,21 @@ export class Color {
     readonly b: number;
     readonly a: number;
 
-    constructor(r: number, g: number, b: number, a: number) {
+    constructor(r: number, g: number, b: number, a: number, dark: boolean = false) {
+        if(dark){
+            if(r<0.3){
+                r = 1 -r;
+            }
+            if(g<0.3){
+                g = 1 -g;
+            }
+            if(b<0.3){
+                b = 1 -b;
+            }
+        }
         this.r = round3(r);
         this.g = round3(g);
         this.b = round3(b);
         this.a = round3(a);
-    }
-
-    fromHex(hex: string) {
-
     }
 }
